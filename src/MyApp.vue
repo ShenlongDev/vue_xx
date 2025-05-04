d74cae6b9fec0493c988524f87c6dbddb8323c7c
SuperDev116 <158676568+SuperDev116@users.noreply.github.com>
2025-05-04 10:40:55 +0900

    raw html

---
+++ src/MyApp.vue
@@ -0,0 +1,16 @@
+<script setup>
+  const msg = 'Hello, Vue3!'
+  const rawHtml = '<span style="color: red">This should be red.</span>'
+</script>
+
+<template>
+  <p>{{ msg }}</p>
+  <p>Using text interpolation: {{ rawHtml }}</p>
+  <p>Using v-html directive: <span v-html="rawHtml"></span></p>
+</template>
+
+<style scoped>
+  p {
+    font-size: 40px;
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -2,5 +2,7 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import MyApp from './MyApp.vue'
 
-createApp(App).mount('#app')
+// createApp(App).mount('#app')
+createApp(MyApp).mount('#app')
