59f55ffbc2a7efae08d46bbc1a17078b81aadbcd
SuperDev116 <158676568+SuperDev116@users.noreply.github.com>
2025-05-05 12:30:39 +0900

    JavaScript expression

--- src/MyApp.vue
+++ src/MyApp.vue
@@ -1,65 +1,28 @@
 <script setup>
-  const msg = 'Hello, Vue3!'
-  const rawHtml = '<span style="color: red">This should be red.</span>'
-  const dynamicId = 'firstBinding'
-  const isDisabled = false
-  const objectOfAttrs = {
-    id: 'oid',
-    class: 'oclass',
-    style: 'background-color: green'
-  }
+  const number = 100
 </script>
 
 <template>
-  <p>{{ msg }}</p>
-  <p>Using text interpolation: {{ rawHtml }}</p>
-  <p>Using v-html directive: <span v-html="rawHtml"></span></p>
-  # Attribute binding
-  <div v-bind:id="dynamicId">
-    This div is for showing attribute binding.
-  </div>
+  使用JavaScript表达式
+  至此，我们进在模板中绑定了一些简单的属性名。
+  但是vue实际上在所有的数据绑定中都支持完整的JavaScript表达式：
 
-  # 简写
-  因为v-bind非常常用，我们提供了特定的简写语法。
-  <div :id="dynamicId">
-    开头为：的attribute可能和一般的HTML attribute 看起来不太一样，但它的确是合法的attribute名称字符，并且所有支持Vue的浏览器都能正确解析它。此外，他们不会出现在最终渲染的DOM中。简写语法是可选的，但相信在你了解它更多的用处后，你应该会更喜欢它。
-  </div>
+  <span>{{ number + 1 }}</span>
 
-  # 同名简写
-  如果attribute的名称与绑定的JavaScript之的名称相同，那么可以进一步简化语法，省略attribute值：
-  这与在JavaScript中声明对象时使用的属性简写语法类似。
+  这些表达式都会被作为Javascript，以当前组件实例为作用域解析举行。
+  在Vue模板内，JavaScript表达式可以被使用在如下场景上：
+  在文本插值中（双大括号）
+  在任何Vue指令（以v-开头的特殊attribute）attribute的值中
 
-  # 布尔型attribute
-  布尔型attribute依据true/false值来决定attribute是否应该存在于该元素上。disabled就是最常见的例子之一。
-  v-bind在这首那个场景下的行为略有不同：
+  仅支持表达式
+  每个绑定仅支持单一表达式，也就是一段能够被求求值的JavaScript代码。
+  一个简单的判断方法是是否可以合法地写在return后面。
+  因此，下面的例子都是无效的
 
-  <button :disabled="isDisabled">布尔型绑定按钮</button>
-
-  # 动态绑定多个值
-  如果你有像这样的一个包含多个attribute的JavaScript对象；
-  通过不带参数的v-bind，你可以将他们绑定到单个元素上。
-  <div v-bind="objectOfAttrs">
-    通过不带参数的v-bind，你可以将他们绑定到单个元素上。
-  </div>
+  调用函数
+  可以在绑定的表达式中使用一个组件暴露的方法
 </template>
 
 <style scoped>
-  p {
-    color: blue;
-    font-size: 20px;
-  }
-
-  div#firstBinding {
-    color: red;
-    font-size: 24px;
-  }
-
-  #oid {
-    color: yellow;
-    font-size: 20px;
-  }
-
-  .oclass {
-    border: 2px solid black;
-  }
+  
 </style>
