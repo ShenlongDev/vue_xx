03a85b45553eb0dafc9a63623d8fb13fada5ab7e
SuperDev116 <158676568+SuperDev116@users.noreply.github.com>
2025-05-06 16:42:53 +0900

    directive dynamic parameter

--- src/MyApp.vue
+++ src/MyApp.vue
@@ -4,6 +4,9 @@
   const doSth = () => {
     alert('干的漂亮，你在听我的事件')
   }
+
+  const attributeName = 'href'
+  const eventName = 'focus'
 </script>
 
 <template>
@@ -20,6 +23,19 @@
   这里href就是一个参数，他告诉v-bind指令将表达式url的值绑定元组的hrefattribute上。在简写中，参数前的一切都会被缩略为一个：字符
   另一个例子是v-
   <a v-on:click="doSth">i你好，我是监听事件的</a>
+  这里的参数是要监听的事件名称：click
+  v-on有一个缩写，即@字符
+  我们之后也会讨论关于时间处理的更多细节。
+
+  同样在指令参数上也剋以使用一个Javascript表达式，需要包含在一对方括号内：
+
+  <a v-bind:[attributeName]="url">这是动态参数的实例</a>
+  <a :[attributeName]="url">这是动态参数的实例的简写</a>
+  这里的attributeName会作为一个JavaScript表达式被动态举行，计算得到的值会被用作最终的参数。
+  如果你的组件实例有一个数据属性attributeName，其职位‘href'，那么这个绑定就等价于v-bind:href。
+  相似地，你可以将一个函数绑定到动态的时间名称上
+
+  <button @[eventName]="doSth">我是动态事件名称</button>
 </template>
 
 <style scoped>
