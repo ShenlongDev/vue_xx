6a767edc81c7910c7cab1b767303b642853cff2b
SuperDev116 <158676568+SuperDev116@users.noreply.github.com>
2025-05-07 18:24:02 +0900

    reactivity nextTick

--- src/MyApp.vue
+++ src/MyApp.vue
@@ -1,29 +1,19 @@
 <script setup>
-import { ref } from 'vue'
+import { nextTick } from 'vue'
 
-const obj = ref({
-  nested: { count: 0 },
-  arr: ['foo', 'bar']
-})
-
-function mutateDeeply() {
-  // 以下都会按照期望工作
-  obj.value.nested.count++
-  obj.value.arr.push('baz')
+async function increment() {
+  count.value++
+  await nextTick()
+  // 现在 DOM 已经更新了
 }
 </script>
 
 <template>
-  深层响应性​
-  Ref 可以持有任何类型的值，包括深层嵌套的对象、数组或者 JavaScript 内置的数据结构，比如 Map。
+  # DOM更新时机
+  当你修改了响应式状态时，DOM会被自动更新。但是要注意的是，DOM更新不是同步的。
+  Vue会在’next tick‘更新周期中缓冲所有状态的修改，以确保不管你进行了多少次状态修改，每个组件都值会被更新一次。
 
-  Ref 会使它的值具有深层响应性。这意味着即使改变嵌套对象或数组时，变化也会被检测到：
-
-  非原始值将通过 reactive() 转换为响应式代理，该函数将在后面讨论。
-
-  也可以通过 shallow ref 来放弃深层响应性。对于浅层 ref，只有 .value 的访问会被追踪。浅层 ref 可以用于避免对大型数据的响应性开销来优化性能、或者有外部库管理其内部状态的情况。
-
-  阅读更多：
+  要等待DOM更行完成后在举行额外的代码，可以使用nextTick()全局API：
 </template>
 
 <style scoped></style>
