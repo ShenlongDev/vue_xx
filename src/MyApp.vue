fb35014135b1e524270edc129eec566b6e33a998
SuperDev116 <158676568+SuperDev116@users.noreply.github.com>
2025-05-07 18:55:10 +0900

    reactive concept

--- src/MyApp.vue
+++ src/MyApp.vue
@@ -1,19 +1,19 @@
 <script setup>
-import { nextTick } from 'vue'
+import { reactive } from 'vue'
 
-async function increment() {
-  count.value++
-  await nextTick()
-  // 现在 DOM 已经更新了
-}
+const state = reactive({ count: 0 })
 </script>
 
 <template>
-  # DOM更新时机
-  当你修改了响应式状态时，DOM会被自动更新。但是要注意的是，DOM更新不是同步的。
-  Vue会在’next tick‘更新周期中缓冲所有状态的修改，以确保不管你进行了多少次状态修改，每个组件都值会被更新一次。
+  <button @click="state.count++">
+    {{ state.count }}
+  </button>
 
-  要等待DOM更行完成后在举行额外的代码，可以使用nextTick()全局API：
+  还有另一种声明响应式状态的方式，及使用reactive()API。
+  与将内布置包装在特殊对象中的ref不同，reactive()将使对象本身具有响应性。
+  响应式对象是JS代理，其行为就和普通对象一样。
+  不同的是Vue能够拦截对响应式对象所有属性的访问和修改，一遍进行以来追踪和触发更新。
+  reactive() 将深层地转换对象：当访问嵌套对象时，它们也会被 reactive() 包装。当 ref 的值是一个对象时，ref() 也会在内部调用它。与浅层 ref 类似，这里也有一个 shallowReactive() API 可以选择退出深层响应性。
 </template>
 
 <style scoped></style>
