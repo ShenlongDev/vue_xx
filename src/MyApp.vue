dbd9f20ee855a86bab15b2652a3ac47e0c7985a6
SuperDev116 <158676568+SuperDev116@users.noreply.github.com>
2025-05-22 10:32:43 +0900

    ArrayAndCollection

--- src/MyApp.vue
+++ src/MyApp.vue
@@ -1,22 +1,13 @@
 <script setup>
 import { ref, reactive } from 'vue'
 
-const count = ref(0)
-const state = reactive({
-  count
-})
+const books = reactive([ref('Vue 3 Guide')])
+// 这里需要 .value
+console.log(books[0].value)
 
-console.log(state.count)
-state.count = 1
-console.log(count.value)
-
-const otherCount = ref(2)
-state.count = otherCount
-console.log(state.count)
-state.count++
-console.log(otherCount.value)
-
-console.log(count.value)
+const map = reactive(new Map([['count', ref(0)]]))
+// 这里需要 .value
+console.log(map.get('count').value)
 </script>
 
 <template>
@@ -25,6 +16,9 @@ console.log(count.value)
   <p>一个ref会在作为响应式对象的属性被访问或修改时自动解包。和换句话说，他的行为就像一个普通的属性：</p>
 
   <p>如果将一个新的ref赋值给一个关联了已有ref的属性，那么他会替换掉旧的ref：</p>
+
+  <h1>数组和集合的注意事项</h1>
+  <p>与reactive对象不同的是，当ref作为响应式数组或原生集合类型（如Map）中的元素被访问时，他不会被解包</p>
 </template>
 
 <style scoped></style>
