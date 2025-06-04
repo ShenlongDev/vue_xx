6bb3629ef9f583bfcb149f25df81fdeb2e0929d9
SuperDev116 <158676568+SuperDev116@users.noreply.github.com>
2025-06-04 11:01:03 +0900

    绑定数组

--- src/MyApp.vue
+++ src/MyApp.vue
@@ -1,41 +1,26 @@
 <script setup>
-import { ref, computed } from 'vue'
+import { ref } from 'vue'
+const activeClass = ref('active')
+const errorClass = ref('text-danger')
 const isActive = ref(true)
-const hasError = ref(false)
-
-const error = ref(null)
-
-const classObject = computed(() => ({
-  active: isActive.value && !error.value,
-  'text-danger': error.value && error.value.type === 'fatal'
-}))
 </script>
 
 <template>
   <h1>类与样式绑定</h1>
   <h2>绑定 HTML class</h2>
-  <h3>绑定对象</h3>
-  <p>我们可以给 :class (v-bind:class 的缩写) 传递一个对象来动态切换 class：</p>
-  <div class="static" :class="{ active: isActive, 'text-danger': hasError }">
-    <p>这里的语法表示active是否存在取决于数据属性isActive的真假值。</p>
-    <p>你可以在对象中写多个字段来操作多个class。</p>
-    <p>此外，：class指令也可以和一般的 class attribute共存。</p>
-    <p>举例来说,下面这样的状态：</p>
-    <p>配合一下模板</p>
-    <div class="static" :class="{ active: isActive, 'text-danger': hasError }"></div>
-    <p>渲染的结果会是：</p>
-    <div class="static active">当isActive或者hasError改变时，class列表会随之更新</div>
-    <p>举例来说，如果hasError变为true，class列表会变成“static active text-danger”。</p>
-    <p>绑定的对象并不一定需要写成内敛字面量的形式，也可以直接绑定一个对象：</p>
+  <h3>绑定数组</h3>
+  <p>我们剋以给：class绑定一个数组来渲染多个CSS class:</p>
+  <div :class="[activeClass, errorClass]"></div>
 
-    <div :class="classObject"></div>
+  <div class="active text-danger">渲染的结果时：</div>
 
-    这将渲染：
-    <div class="active"></div>
+  <div :class="[isActive ? activeClass : '', errorClass]">
+    如果你也想在数组中有条件地渲染某个class，你可以使用三元表达式：
+  </div>
 
-    我们也可以绑定一个返回对象的计算属性。这是一个常见且很有用的技巧：
-
-    <div :class="classObject"></div>
+  <div :class="[{ [activeClass]: isActive }, errorClass ]">
+    errorClass会一直存在， 但activeClass之会在isActive为真时才存在。
+    然而，这可能在有多个以来条件的class时会有些冗长。因此也可以在数组中嵌套对象。
   </div>
 </template>
 
