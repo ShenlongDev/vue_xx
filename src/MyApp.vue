b82add6d11377169e8b1dcd6064feebaa81d2584
SuperDev116 <158676568+SuperDev116@users.noreply.github.com>
2025-06-05 12:25:08 +0900

    绑定内联样式-2

--- src/MyApp.vue
+++ src/MyApp.vue
@@ -1,7 +1,11 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, reactive } from 'vue'
 const activeColor = ref('red')
 const fontSize = ref(30)
+const sstyleObject = reactive({
+  color: 'red',
+  fontSize: '30px'
+})
 </script>
 
 <template>
@@ -13,6 +17,17 @@ const fontSize = ref(30)
     <p>尽管推荐使用camelCase，但：style也支持kebab-cased形式的CSS属性key（对应其CSS中的实际名称），例如：</p>
     <div :style="{ 'font-size': fontSize + 'px' }">我是kebab-case</div>
   </div>
+  <p>直接绑定一个样式对象通常是一个好主意，这样可以使模板更加简洁</p>
+
+  <div :style="sstyleObject">
+    <p>同样的，如果样式对象需要更复杂的逻辑，也可以使用返回样式对象的计算属性。</p>
+    <p>：style指令也可以和常规的 style attribute共存，就像：class。</p>
+  </div>
+
+  模板
+  <h1 style="color: red" :style="'font-size: 1em'">hello</h1>
+  这将被渲染为：
+  <h1 style="color: red; font-size: 1em;">hello</h1>
 </template>
 
 <style scoped></style>
