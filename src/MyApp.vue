0691ca868b05bff701810b02997cac6380062f21
SuperDev116 <158676568+SuperDev116@users.noreply.github.com>
2025-06-05 12:18:41 +0900

    绑定内联样式-1

--- src/MyApp.vue
+++ src/MyApp.vue
@@ -1,43 +1,18 @@
 <script setup>
 import { ref } from 'vue'
-const isActive = ref(true)
+const activeColor = ref('red')
+const fontSize = ref(30)
 </script>
 
 <template>
   <h1>类与样式绑定</h1>
-  <h2>绑定 HTML class</h2>
-  <h3>在组件上使用</h3>
-  <p>本届假设你已经有Vue组件的知识基础。如果没有，你也可以暂时跳过，以后再阅读</p>
-  对于只有一个根元素的组件，当你使用了class sttribute时，这些class会被添加到跟元素上并与该元素上已有的class合并。
-  举例来说，如果你生命了一个组件名叫MyComponent，模板如下：
-  <!-- 字组件模板 -->
-  <p class="foo bar">Hi!</p>
-  在使用时添加一些class：
-  <!-- 在使用组件时 -->
-  <MyComponent class="baz boo" />
-
-  渲染的HTML为：
-  <p class="foo bar baz boo">这是被渲染的HTML</p>
-
-  Class的绑定也是同样的：
-  <MyComponent :class="{ active: isActive }" />
-
-  当isActive为真时，被渲染的HTML会是：
-  <p class="foo bar active">Hi!</p>
-
-  如果你的组件有多个根元素，你将需要指定那个根元素来接受这个class。
-  你可以通过组件的 $attrs属性来指定接受的元素：
-
-  <!-- MyComponent 模板使用 $attrs 时 -->
-  <p :class="$attrs.class">Hi!</p>
-  <span>This is a child component</span>
-  <MyComponent class="baz" />
-
-  这将被渲染为：
-  <p class="baz">Hi!</p>
-  <span>This is a child component</span>
-
-  你可以在透传Attribute一章中了解更多组件的attribute继承的细节
+  <h2>绑定内联样式</h2>
+  <h3>绑定对象</h3>
+  <p>:style支持绑定JavaScript对象值，对应的是HTML元素的style属性：</p>
+  <div :style="{ color: activeColor, fontSize: fontSize + 'px' }">
+    <p>尽管推荐使用camelCase，但：style也支持kebab-cased形式的CSS属性key（对应其CSS中的实际名称），例如：</p>
+    <div :style="{ 'font-size': fontSize + 'px' }">我是kebab-case</div>
+  </div>
 </template>
 
 <style scoped></style>
