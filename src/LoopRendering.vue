a5e2977f9d0909775d6a2d754530e373598655d1
SuperDev116 <158676568+SuperDev116@users.noreply.github.com>
2025-06-11 14:22:56 +0900

    v-for2

--- src/LoopRendering.vue
+++ src/LoopRendering.vue
@@ -1,6 +1,7 @@
 <script setup>
 import CodeView from './components/CodeView.vue';
 import HeadingTwo from './components/HeadingTwo.vue';
+import EnLetter from './components/EnLetter.vue';
 </script>
 
 <template>
@@ -11,5 +12,13 @@ import HeadingTwo from './components/HeadingTwo.vue';
   <CodeView :content="`const items = ref([{ message: 'Foo' }, { message: 'Bar' }])`" />
   <CodeView :content='`<li v-for="item in items">
   {{ item.message }}
+</li>`' />
+
+  <p>在 <EnLetter content="v-for" /> 块中可以完整地访问父作用域内的属性和变量。<EnLetter content="v-for" />也支持使用可选的第二个参数表示当前想的位置索引。</p>
+
+  <CodeView :content="`const parentMessage = ref('Parent')
+const items = ref([{ message: 'Foo' }, { message: 'Bar' }])`" />
+  <CodeView :content='`<li v-for="(item, index) in items">
+  {{ parentMessage }} - {{ index }} - {{ item.message }}
 </li>`' />
 </template>
