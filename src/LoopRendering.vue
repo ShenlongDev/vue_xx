8145e8edbbc54d82027fdee8469bf9f75158c41f
SuperDev116 <158676568+SuperDev116@users.noreply.github.com>
2025-06-18 18:49:59 +0900

    v-for_through_key

--- src/LoopRendering.vue
+++ src/LoopRendering.vue
@@ -126,4 +126,16 @@ items.forEach((item, index) => {
     {{ todo.name }}
   </li>
 </template>`' />
+
+<HeadingTwo heading="通过key管理状态" />
+<p>Vue默认按照”就地更新“的策略来更新通过 v-for 渲染的元素列表。当数据顶的顺序改变时，Vue不会随之移动 DOM 元素的顺序，而是就地更新每个元素，确保他们在原本是定的索引位置上渲染。</p>
+<p>默认模式是高效的，但只适用于列表渲染输出的结果不依赖子组件状态或者临时 DOM 状态 (例如表单输入值) 的情况。</p>
+<p>为了给 Vue 一个提示，以便它可以跟踪每个节点的标识，从而重用和重新排序现有的元素，你需要为每个元素对应的块提供一个唯一的 key attribute：</p>
+<CodeView :content='`<div v-for="item in items" :key="item.id">
+  <!-- 内容 -->
+</div>`' />
+<p>当你使用 <EnLetter content="<template v-for>" /> 时，key 应该被放置在这个 <EnLetter content="<template>" /> 容器上：</p>
+<CodeView :content='`当你使用 <template v-for> 时，key 应该被放置在这个 <template> 容器上：`' />
+  <p>推荐在任何可行的时候为 v-for 提供一个 key attribute，除非所迭代的 DOM 内容非常简单 (例如：不包含组件或有状态的 DOM 元素)，或者你想有意采用默认行为来提高性能。</p>
+  <p>key 绑定的值期望是一个基础类型的值，例如字符串或 number 类型。不要用对象作为 v-for 的 key。关于 key attribute 的更多用途细节，请参阅 key API 文档。</p>
 </template>
