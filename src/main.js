e5dd7c9655eed142d08e59143a8a391636423232
SuperDev116 <158676568+SuperDev116@users.noreply.github.com>
2025-06-22 23:32:19 +0900

    component_intro

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
-import App from './App.vue'
-import MyApp from './MyApp.vue'
+import App from './ComponentBasic.vue'
 
-// createApp(App).mount('#app')
-createApp(MyApp).mount('#app')
+createApp(App).mount('#app')
