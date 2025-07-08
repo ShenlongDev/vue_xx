0f40ea9a502413a91001bc732683e435c6e5a5ad
ShenlongDev <super0116.dev@gmail.com>
2025-07-08 21:24:26 +0900

    深层侦听器3

--- src/WatchersVue.vue
+++ src/WatchersVue.vue
@@ -59,9 +59,11 @@ obj.count++
 
 watch(
   () => obj.count,
-  () => {
-    // 仅当 state.someObject 被替换时触发
-  }
+  (newValue, oldValue) => {
+    // 注意：`newValue` 此处和 `oldValue` 是相等的
+    // *除非* state.someObject 被整个替换了
+  },
+  { deep: true }
 )
 </script>
 
@@ -97,5 +99,7 @@ watch(
   <h2>深层侦听器</h2>
   <p>直接给 watch() 传入一个响应式对象，会隐式地创建一个深层侦听器——该回调函数在所有嵌套的变更时都会被触发：</p>
   <p>相比之下，一个返回响应式对象的 getter 函数，只有在返回不同的对象是，才会触发回调：</p>
+  <p>你也可以给上面这个例子显式地加上 deep 选项，强制转成深层侦听器：</p>
+  <p>在 Vue 3.5+ 中，deep 选项还可以是一个数字，表示最大遍历深度——即 Vue 应该遍历对象嵌套属性的级数。</p>
 </template>
 
