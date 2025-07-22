62606c13bffb004c148a44f127932229cac28536
ShenlongDev <super0116.dev@gmail.com>
2025-07-22 22:00:03 +0900

    docs: 全局注册2
    导入单文件组件注册组件

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,22 @@
 <script setup>
 import { createApp } from 'vue'
+import ComponentRegister from './feature/ComponentRegister.vue'
 
 const app = createApp({})
 
-app.component(
-  // 组件的名字
-  'ComponentRegister',
-  // 组件的实现
-  {
-  template: `
-    <p>这是全局注册实例</p>
-  `
-})
+// app.component(
+//   // 组件的名字
+//   'ComponentRegister',
+//   // 组件的实现
+//   {
+//   template: `
+//     <p>这是全局注册实例</p>
+//   `
+// })
+
+app.component('ComponentRegister', ComponentRegister)
 </script>
+
+<template>
+  <ComponentRegister />
+</template>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 
-import App from 'App.vue'
+import App from './App.vue'
 
 createApp(App).mount('#app')
