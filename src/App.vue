8ab7db8b96fd187c8410af188c6229289e658f3e
ShenlongDev <super0116.dev@gmail.com>
2025-07-22 23:02:38 +0900

    docs: 全局注册3
    - 链式调用
    - 彼此内部使用

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,25 @@
 <script setup>
 import { createApp } from 'vue'
 import ComponentRegister from './feature/ComponentRegister.vue'
+import ComponentBasic from './ComponentBasic.vue'
 
 const app = createApp({})
 
-// app.component(
+app
+// .component(
 //   // 组件的名字
-//   'ComponentRegister',
+//   'ComponentTest',
 //   // 组件的实现
 //   {
 //   template: `
 //     <p>这是全局注册实例</p>
 //   `
 // })
-
-app.component('ComponentRegister', ComponentRegister)
+.component('ComponentRegister', ComponentRegister)
+.component('ComponentBasic', ComponentBasic)
 </script>
 
 <template>
   <ComponentRegister />
+  <ComponentBasic />
 </template>
