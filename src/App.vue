0e9d6957102aa64c8e291770dbe8266000a2443a
ShenlongDev <super0116.dev@gmail.com>
2025-07-23 18:33:06 +0900

    docs: 局部注册2
    - 局部注册组件

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,23 @@
-<script setup>
+<!-- <script setup>
 import { createApp } from 'vue'
 import ComponentRegister from './feature/ComponentRegister.vue'
 
 const app = createApp({})
 
 app.component('ComponentRegister', ComponentRegister)
+</script>
+
+<template>
+  <ComponentRegister />
+</template> -->
+<script>
+import ComponentRegister from './feature/ComponentRegister.vue'
+
+export default {
+  components: {
+    ComponentRegister
+  }
+}
 </script>
 
 <template>
