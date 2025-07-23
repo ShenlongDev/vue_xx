18b8e6d416f62a07f97da72a74f85b3953133b6f
ShenlongDev <super0116.dev@gmail.com>
2025-07-23 18:25:44 +0900

    docs: 局部注册1
    - 全局注册的坏处
    - 局部注册的有点

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,12 @@
 <script setup>
 import { createApp } from 'vue'
 import ComponentRegister from './feature/ComponentRegister.vue'
-import ComponentBasic from './ComponentBasic.vue'
 
 const app = createApp({})
 
-app
-// .component(
-//   // 组件的名字
-//   'ComponentTest',
-//   // 组件的实现
-//   {
-//   template: `
-//     <p>这是全局注册实例</p>
-//   `
-// })
-.component('ComponentRegister', ComponentRegister)
-.component('ComponentBasic', ComponentBasic)
+app.component('ComponentRegister', ComponentRegister)
 </script>
 
 <template>
   <ComponentRegister />
-  <ComponentBasic />
 </template>
