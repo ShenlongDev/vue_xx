678401d639be615fe7fedb6df2b86123c0dc9732
ShenlongDev <super0116.dev@gmail.com>
2025-07-24 16:49:46 +0900

    docs: Props声明1
    - 声明 Props 两种方式
    - 定义 Props

---
+++ src/views/docs_component/ComponentProps.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>Props</h1>
+  <h2>Props 声明</h2>
+  <p>一个组件需要显式声明它所接受的 props，这样 Vue 才能知道外部传入的哪些是 props，哪些是透传 attribute。</p>
+  <p>在使用 script setup 的单文件组件中，props可以使用 defineProps() 宏来声明。</p>
+  <p>在没有使用 script setup 的组件中，props 可以用 props 选项来声明：</p>
+</template>
+
+<!-- <script setup>
+const props = defineProps(['foo'])
+
+console.log(props.foo)
+</script> -->
+<script>
+export default {
+  props: ['foo'],
+  setup(props) {
+    console.log(props.foo)
+  }
+}
+</script>
