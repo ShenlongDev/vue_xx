ca48225a1364c664580f4870b9a72abd0cfc52d8
ShenlongDev <super0116.dev@gmail.com>
2025-07-25 16:31:01 +0900

    docs: 响应式 props 解构3
    - 传递 prop 到函数

--- src/views/docs_component/ComponentProps.vue
+++ src/views/docs_component/ComponentProps.vue
@@ -6,21 +6,18 @@
   <p> 3.4 及以下版本，foo 是一个实际的常量，永远不会改变。在 3.5 及以上版本，当在同一个 script setup 代码块中访问由 defineProps 解构的变量时，Vue 编译器会自动在前面添加 props.。因此，上面的代码等同于以下代码：</p>
   <p>此外，你可以使用 JavaScript 原生的默认值语法声明 Props 默认值。这在使用基于类型的 props 声明时特别有用。</p>
   <p>如果你希望在 IDE 中在解构的 props 和普通变量之间有更多视觉上的区分，Vue 的 VSCode 扩展提供了一个设置来启用解构 props 的内联提示。</p>
+
+  <h3>将解构的 props 传递到函数中</h3>
+  <p>当我们将解构的 prop 传递到函数中时，例如：</p>
+  <p>此外，当我们需要传递解构的 prop 到外部函数中并保持响应性时，这是推荐做法：</p>
 </template>
 
 <script setup>
-import { watchEffect } from 'vue'
-// const { foo } = defineProps(['foo'])
+import { watch, useComposable } from 'vue'
 
-// watchEffect(() => {
-//   // 在 3.5 之前只运行一次
-//   // 在 3.5+ 中在 "foo" prop 变化时重新执行
-//   console.log(foo)
-// })
-const props = defineProps(['foo'])
-
-watchEffect(() => {
-  // `foo` 由编译器转换为 `props.foo`
-  console.log(props.foo)
+const { foo } = defineProps(['foo'])
+watch(() => foo, () => {
+  // blabla
 })
+useComposable(() => foo)
 </script>
