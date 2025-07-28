c94723c49af972d5baf22794e3233df262bb1675
ShenlongDev <super0116.dev@gmail.com>
2025-07-28 22:35:12 +0900

    docs: 传递prop的细节2
    - 解释静态 props
    - 解释动态 props

--- src/views/docs_component/ComponentProps.vue
+++ src/views/docs_component/ComponentProps.vue
@@ -2,23 +2,19 @@
   <h1>Props</h1>
   <h2>传递 prop 的细节</h2>
   <h3>Prop 名字格式</h3>
-  <p>如果一个 prop 的名字很长，应使用 camelCase 形式，因为它们是合法的 JavaScript 标识符，可以直接在模板的表达式中使用，也可以避免在作为属性 key 名时必须加上引号。</p>
-  <span>{{ greetingMessage }}</span>
-  <p>虽然理论上你也可以在向子组件传递 props 时使用 camelCase 形式，但实际上为了和 HTML attribute 对齐，我们通常会将其写为 kebab-case 的形式：</p>
-  <MyComponent greeting-message="hello" />
-  <p>对组件名我们推荐使用 PascalCase，因为这提高了模板可读性，能帮助我们区分 Vue 组件和原生 HTML 元素。然而对于传递 props 来说，使用 camelCase 并没有太多的优势，因此我们推荐更贴近 HTML 的书写风格。</p>
+  <h3>静态 vs. 动态 Props</h3>
+  <p>至此，你已经见过了很多像这样的静态值形式的 Props：</p>
+  <PostBlog title="My journey with Vue" />
+  <p>相应地，还有使用 v-bind 或缩写：来进行动态绑定的 props：</p>
+  <PostBlog :title="post.title + '.'" />
+  <PostBlog :title="post.title + ' by ' + post.author + '.'" />
 </template>
 
 <script setup>
-const props = defineProps({
-  greetingMessage: String
-})
+import PostBlog from '@/components/PostBlog.vue'
 
-// 定义子组件
-const MyComponent = {
-  props: {
-    greetingMessage: String
-  },
-  template: `<span>hello, {{ greetingMessage }}</span>`
+const post = {
+  title: 'This is the first post',
+  author: 'super dev'
 }
 </script>
