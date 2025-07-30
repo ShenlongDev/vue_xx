1d1f9bb34c4f2b7057a14d26ca08a24f817446a2
ShenlongDev <super0116.dev@gmail.com>
2025-07-30 22:04:16 +0900

    docs: 单向数据流 2

--- src/views/docs_component/ComponentProps.vue
+++ src/views/docs_component/ComponentProps.vue
@@ -3,11 +3,24 @@
   <h2>单向数据流</h2>
   <p>所有的 props 都遵循着单向绑定原则，props 因父组件的更新而变化，自然地将新的状态向下流往子组件，而不会逆向传递。这避免了子组件意外修改父组件的状态的情况，不然应用的数据流将很容易得混乱而难以理解。</p>
   <p>另外，每次父组件更新后，所有的子组件中的 props 都会被更新到最新值，这意味着你不应该在子组件中去更改一个 prop。若你这么做了，Vue 会在控制台向你抛出警告：</p>
+  <p>导致你想要更改一个 prop 的需求通常来源于以下两种场景：</p>
+  <p>prop 被用于传入初始值；而子组件想在之后将其作为一个局部数据属性。在这种情况下，最好是新定义一个局部数据属性，从 props 上获取初始值即可：</p>
+  <p>需要对传入的 prop 值做进一步的转换。在这种情况中，最好是基于该 prop 值定义一个计算属性：</p>
 </template>
 
 <script setup>
-const props = defineProps(['foo'])
+// const props = defineProps(['foo'])
 
-// ❌ 警告！prop 是只读的！
-props.foo = 'bar'
+// // ❌ 警告！prop 是只读的！
+// props.foo = 'bar'
+
+const props = defineProps(['initialCounter'])
+
+// 计数器只是将 props.initialCounter 作为初始值
+// 像下面这样做就使 prop 和后续更新无关了
+const counter = ref(props.initialCounter)
+const props = defineProps(['size'])
+
+// 该 prop 变更时计算属性也会自动更新
+const normalizedSize = computed(() => props.size.trim().toLowerCase())
 </script>
