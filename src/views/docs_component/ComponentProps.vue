24b242387f802db051fe4399d2074a6ce1e50e9a
ShenlongDev <super0116.dev@gmail.com>
2025-07-30 22:00:08 +0900

    docs: 单向数据流 1

--- src/views/docs_component/ComponentProps.vue
+++ src/views/docs_component/ComponentProps.vue
@@ -1,29 +1,13 @@
 <template>
   <h1>Props</h1>
-  <h2>传递 prop 的细节</h2>
-  <h3>Prop 名字格式</h3>
-  <h3>静态 vs. 动态 Props</h3>
-  <h3>传递不同的值类型</h3>
-  <p>在上述的两个例子中，我们只传入了字符串值，但实际上任何类型的值都可以作为 props 的值被传递。</p>
-  <h4>Number</h4>
-  <h4>Boolean</h4>
-  <h4>Array</h4>
-  <h4>Object</h4>
-  <h3>使用一个对象绑定多个 prop</h3>
-  <p>如果你想要将一个对象的所有属性当做 props 传入，你可以使用没有参数的 v-bind，机制使用 v-bind 而非：prop-name。例如，这里有一个 post 对象：</p>
-  <post-blog v-bind="post"></post-blog>
+  <h2>单向数据流</h2>
+  <p>所有的 props 都遵循着单向绑定原则，props 因父组件的更新而变化，自然地将新的状态向下流往子组件，而不会逆向传递。这避免了子组件意外修改父组件的状态的情况，不然应用的数据流将很容易得混乱而难以理解。</p>
+  <p>另外，每次父组件更新后，所有的子组件中的 props 都会被更新到最新值，这意味着你不应该在子组件中去更改一个 prop。若你这么做了，Vue 会在控制台向你抛出警告：</p>
 </template>
 
 <script setup>
-import PostBlog from '@/components/PostBlog.vue'
+const props = defineProps(['foo'])
 
-const post = {
-  title: 'This is the first post',
-  likes: 5003,
-  commentIds: [234, 266, 273],
-  author: {
-    name: 'Veronica',
-    company: 'Veridian Dynamics'
-  }
-}
+// ❌ 警告！prop 是只读的！
+props.foo = 'bar'
 </script>
