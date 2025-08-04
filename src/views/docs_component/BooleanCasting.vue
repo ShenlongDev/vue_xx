bda480392d02363a2c884797202f50cc5e30490f
ShenlongDev <super0116.dev@gmail.com>
2025-08-04 15:36:52 +0900

    docs: Boolean 类型转换2

--- src/views/docs_component/BooleanCasting.vue
+++ src/views/docs_component/BooleanCasting.vue
@@ -6,10 +6,27 @@
 
   <!-- 等同于传入 :disabled="false" -->
   <MyComponent />
+  <p>当一个 prop 被声明为允许多种类型时，Boolean 的转换规则也将被应用。然而，当同时允许 String 和 Boolean 时，有一种边缘情况——只有当 Boolean 出现在 String 之前时，Boolean 转换规则才适用：</p>
 </template>
 
 <script setup>
+// disabled 将被转换为 true
 defineProps({
-  disabled: Boolean
+  disabled: [Boolean, Number]
+})
+
+// disabled 将被转换为 true
+defineProps({
+  disabled: [Boolean, String]
+})
+
+// disabled 将被转换为 true
+defineProps({
+  disabled: [Number, Boolean]
+})
+
+// disabled 将被解析为空字符串 (disabled="")
+defineProps({
+  disabled: [String, Boolean]
 })
 </script>
